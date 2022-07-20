df0808e0bd66f6f36180e66bcf2703a41ceb3d10
Bryan Alexander Condor De La Cruz <bryanalexandercd@gmail.com>
2022-07-20 00:17:04 -0500

    fix to parse array with primitives values

--- src/parseToJson.ts
+++ src/parseToJson.ts
@@ -2,15 +2,21 @@ export const parseToJson = (content: string) => {
     const removeClassNameRegex = new RegExp('\\w+(?={)','g');
     const addQuotationMarkToKeys = new RegExp('(?<=[{\\s])([a-zA-Z0-9]+)=','g');
     const addQuotationMarkToValues = new RegExp('(?<=":\\s)([^{}"\\[]+)(?=[,}])','g');
-    const addQuotationMarkToSomeValueInArrays = new RegExp('((?<=":\\s\\[)([^}"]+)(?=[\\]]))','g');
+    const addQuotationMarkToSomeValueInArrays = new RegExp('(?<=":\\s\\[)[^\\{\\}]+(?=\\])','g');
     const removeQuotationMarkOfBooleanValues = new RegExp('"(true|false)"','g');
 
     const contentInJson = content
     .replace(removeClassNameRegex, '')
     .replace(addQuotationMarkToKeys,(match, group1, offset, string)=> `"${group1}": `)
     .replace(addQuotationMarkToValues,(match, group1, offset, string)=> `"${group1}"`)
-    .replace(addQuotationMarkToSomeValueInArrays,(match, group1, offset, string)=> `"${group1}"`)
+    .replace(addQuotationMarkToSomeValueInArrays,(match, offset, string) => parseArrayOfStrings(match))
     .replace(removeQuotationMarkOfBooleanValues,(match, group1, offset, string)=> group1);
     
     return contentInJson;
 };
+
+const parseArrayOfStrings = (value: string = '') => {
+    return value.split(",")
+    .map((value: string) => `"${value}"`)
+    .join(",");
+}; 
